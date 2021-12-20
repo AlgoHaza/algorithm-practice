82e30cc5265a7089903c30019789761621068308
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-21 08:22:35 +0900

    [ADD] 구구단 해결

---
+++ src/baekjoon/_2739_구구단/Main.java
@@ -0,0 +1,15 @@
+package baekjoon._2739_구구단;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d\n", n, i, n * i);
+        }
+    }
+}
