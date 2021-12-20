a8d5af9fd3e962ef11b6d3506fe3e063941d57a8
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-21 08:05:23 +0900

    [ADD] 시험 성적 해결

---
+++ src/baekjoon/_9498_시험_성적/Main.java
@@ -0,0 +1,28 @@
+package baekjoon._9498_시험_성적;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        switch (score / 10) {
+            case 9: case 10:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
+    }
+}
