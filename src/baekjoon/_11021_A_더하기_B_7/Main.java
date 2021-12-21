39f583e43120e463ab1b359a7200174371a56463
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-21 09:05:08 +0900

    [ADD] A+B - 7 해결

---
+++ src/baekjoon/_11021_A_더하기_B_7/Main.java
@@ -0,0 +1,21 @@
+package baekjoon._11021_A_더하기_B_7;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= t; i++) {
+            int sum = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sum();
+            bw.write(String.format("Case #%d: %d\n", i, sum));
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
