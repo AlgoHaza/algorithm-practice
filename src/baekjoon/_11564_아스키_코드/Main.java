ed592830bdf7aef1d48d9562016575333152ff8e
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-21 20:15:26 +0900

    [ADD] 아스키 코드 해결

---
+++ src/baekjoon/_11564_아스키_코드/Main.java
@@ -0,0 +1,10 @@
+package baekjoon._11564_아스키_코드;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d", (int) sc.next().charAt(0));
+    }
+}
