01abb237c691f436677b1b5ea6cf4b9f9b9be165
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-22 17:01:39 +0900

    [ADD] 달팽이는 올라가고 싶다 해결

---
+++ src/baekjoon/_2869_달팽이는_올라가고_싶다/Main.java
@@ -0,0 +1,21 @@
+package baekjoon._2869_달팽이는_올라가고_싶다;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] data = br.readLine().split(" ");
+        int a = Integer.parseInt(data[0]);
+        int b = Integer.parseInt(data[1]);
+        int v = Integer.parseInt(data[2]);
+
+        int currentV = a;
+        int currentDay = 1;
+
+        currentDay += Math.ceil((v - currentV) / (double)(a - b));
+        System.out.println(currentDay);
+    }
+}
