780704790d6827098c9e5e83dd55318d7bad0760
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-22 21:01:42 +0900

    [ADD] A+B - 2 해결

---
+++ src/baekjoon/_2558_A_더하기_B_2/Main.java
@@ -0,0 +1,10 @@
+package baekjoon._2558_A_더하기_B_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
