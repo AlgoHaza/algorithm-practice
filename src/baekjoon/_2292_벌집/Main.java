99e87c0a9a131977d8406d0fc56ec42eb1bef94a
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-22 12:37:49 +0900

    [ADD] 벌집 해결

---
+++ src/baekjoon/_2292_벌집/Main.java
@@ -0,0 +1,29 @@
+package baekjoon._2292_벌집;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    private static Map<Integer, Integer> dp = new HashMap<>();
+
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        int d = 0;
+        while (true) {
+            d++;
+            if(f(d - 1) < n && n <= f(d)) {
+                System.out.println(d);
+                return;
+            }
+        }
+    }
+
+    public static int f(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(!dp.containsKey(n))
+            dp.put(n, f(n-1) + (n-1) * 6);
+        return dp.get(n);
+    }
+}
