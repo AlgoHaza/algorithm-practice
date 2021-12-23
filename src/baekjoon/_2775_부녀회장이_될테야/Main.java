26ea9f0e45433c6d23344dbcb161b3239d83ca03
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-23 09:54:45 +0900

    [ADD] 부녀회장이 될테야 해결

---
+++ src/baekjoon/_2775_부녀회장이_될테야/Main.java
@@ -0,0 +1,30 @@
+package baekjoon._2775_부녀회장이_될테야;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int[][] map = new int[15][15];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int k = sc.nextInt();
+            int n = sc.nextInt();
+            for (int[] ints : map)
+                Arrays.fill(ints, -1);
+            f(14, 14);
+
+            System.out.println(map[k][n]);
+        }
+
+    }
+
+    public static int f(int a, int b) {
+        if(a == 0) map[0][b] = b;
+        if(b == 0) map[a][0] = 0;
+        else if(map[a][b] == -1)
+            map[a][b] = f(a-1, b) + f(a, b-1); //IntStream.rangeClosed(0, b).map(k -> f(a - 1, k)).sum();
+        return map[a][b];
+    }
+}
